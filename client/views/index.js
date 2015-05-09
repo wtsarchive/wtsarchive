bfaec1f9c18565b8095b03fcea35924f29cb1543
wtsarchive <wtsarchive@gmail.com>
2015-05-09 22:05:30 +0200

    Admin panel template placeholders

--- client/views/index.js
+++ client/views/index.js
@@ -2,6 +2,10 @@ Template.layout.helpers({
     sidebarTemplate: function() {
         var currentRoute = Router.current().route.getName();
 
+        // Admin sidebar
+        if (currentRoute.indexOf("admin") === 0)
+            return "sidebarAdmin";
+
         // Default sidebar template
         return "sidebarHome";
     }
