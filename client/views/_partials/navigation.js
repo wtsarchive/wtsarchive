cdbfcf4b65952fca922db57a2c2f026e07d99b54
wtsarchive <wtsarchive@gmail.com>
2015-05-25 18:05:26 +0200

    Style changes, making the site work on small screens

--- client/views/_partials/navigation.js
+++ client/views/_partials/navigation.js
@@ -1,5 +1,12 @@
-Template.navigation.helpers({
+Template.navigationLinks.helpers({
     active: function(item) {
         return (Router.current().route.getName() == item) ? "active" : "";
+    }
+});
+
+Template.navigation.events({
+    "click .navbar-toggle": function() {
+        $('#sidebar').toggle("fast");
+        $('#sidebar').toggleClass("on");
     }
 });
