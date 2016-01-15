6ec25432500c5261c2e4ad0eb43b8ac06b8912cd
wtsarchive <wtsarchive@gmail.com>
2016-01-15 17:40:40 +0100

    Refactor pubs/letters list

--- client/views/letters/list.js
+++ client/views/letters/list.js
@@ -1,47 +1,3 @@
-Template.lettersList.onRendered(function() {
-  var controller = Iron.controller();
-  var years = controller.state.get('years');
-  Session.set("filterYear", _.keys(years)[0]); 
-});
-
-Template.lettersList.helpers({
-  notFound: function() {
-    var category = Categories.findOne({slug: this.slug});
-    return !category && Subscription.ready("categories");
-  },
-  category: function() {
-    if (!Subscription.ready("categories")) return;
-    var category = Categories.findOne({slug: this.slug});
-    return category;
-  },
-  items: function() {
-    if (!Subscription.ready("categories")) return;
-    var category = Categories.findOne({slug: this.slug});
-
-    var start = new Date(Session.get("filterYear"), 1, 1);
-    var end = new Date(Session.get("filterYear"), 12, 30);
-    return Items.find({category: category._id, language: Session.get("language"), published_on: {$gte: start, $lte: end}});
-  },
-  years: function() {
-    var controller = Iron.controller();
-    var years = controller.state.get('years');
-
-    var yearsArray = [];
-    var currentYear = Session.get("filterYear");
-    _.keys(years).forEach(function(year) {
-      yearsArray.push({year: year, selected: currentYear == year});
-    });
-    return yearsArray;
-  }
-});
-
-Template.lettersList.events({
-    "change #items-year": function(e) {
-        var year = $(e.currentTarget).val();
-        Session.set("filterYear", year);
-    },
-});
-
 Template.lettersListItem.helpers({
     image: function() {
         return Covers.findOne(this.cover);
@@ -50,4 +6,3 @@ Template.lettersListItem.helpers({
         return moment(this.published_on).format(Session.get("fullDateFormat"));
     }
 });
-
--- client/views/publications/list.js
+++ client/views/publications/list.js
@@ -1,50 +1,5 @@
-Template.pubsList.onRendered(function() {
-  var controller = Iron.controller();
-  var years = controller.state.get('years');
-  Session.set("filterYear", _.keys(years)[0]); 
-});
-
-Template.pubsList.helpers({
-  notFound: function() {
-    var category = Categories.findOne({slug: this.slug});
-    return !category && Subscription.ready("categories");
-  },
-  category: function() {
-    if (!Subscription.ready("categories")) return;
-    var category = Categories.findOne({slug: this.slug});
-    return category;
-  },
-  items: function() {
-    if (!Subscription.ready("categories")) return;
-    var category = Categories.findOne({slug: this.slug});
-
-    var start = new Date(Session.get("filterYear"), 0, 1);
-    var end = new Date(Session.get("filterYear"), 11, 30);
-    return Items.find({category: category._id, language: Session.get("language"), published_on: {$gte: start, $lte: end}});
-  },
-  years: function() {
-    var controller = Iron.controller();
-    var years = controller.state.get('years');
-
-    var yearsArray = [];
-    var currentYear = Session.get("filterYear");
-    _.keys(years).forEach(function(year) {
-      yearsArray.push({year: year, selected: currentYear == year});
-    });
-    return yearsArray;
-  }
-});
-
-Template.pubsList.events({
-    "change #items-year": function(e) {
-        var year = $(e.currentTarget).val();
-        Session.set("filterYear", year);
-    },
-});
-
 Template.pubsListItem.helpers({
     image: function() {
         return Covers.findOne(this.cover);
     }
 });
-
