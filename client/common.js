e5a23cf8c848b3ab220b01b152bd2a2633f8c56d
wtsarchive <wtsarchive@gmail.com>
2016-01-24 21:37:10 +0100

    Remove reduntant items-recent subscription

--- client/common.js
+++ client/common.js
@@ -95,8 +95,6 @@ Page = {
 Deps.autorun(function() {
     var lang = Session.get("language");
     accountsUIBootstrap3.setLanguage(lang);
-
-    Meteor.subscribe("items-recent", Session.get("language"));
 });
 
 // Detect language
