6222afb17f4c9dc5b1fbf493ee5217c369a240a2
wtsarchive <wtsarchive@gmail.com>
2016-01-25 18:07:03 +0100

    Admin item list sorting, return to list after add, remove code in item list

--- client/views/admin/items/add.js
+++ client/views/admin/items/add.js
@@ -3,19 +3,11 @@ var previousValues = {};
 AutoForm.hooks({
   addItemForm: {
     beginSubmit: function() {
-      previousValues.type = $('select[name=type]').val();
-      previousValues.category = $('select[name=category]').val();
-      previousValues.language = $('select[name=language]').val();
+      Session.set('admin.addItem.type', $('select[name=type]').val());
+      Session.set('admin.addItem.category', $('select[name=category]').val());
     },
     onSuccess: function() {
-      $.bootstrapGrowl("Item added successfully.", {
-        type: 'success'
-      });
-      window.scrollTo(0, 0);
-
-      $('select[name=language]').val(previousValues.language).trigger('change');
-      $('select[name=type]').val(previousValues.type).trigger('change');
-      $('select[name=category]').val(previousValues.category).trigger('change');
+      Router.go('admin.items.list');
     },
     onError: function(form, error) {
       $.bootstrapGrowl("There was an error when trying to insert this item: " + error, {
@@ -31,5 +23,12 @@ Template.adminItemsAdd.onRendered(function() {
   var adminLang = Session.get('adminSelectedLanguage');
   if (languageSelect && adminLang) {
     languageSelect.val(adminLang);
+  }
+
+  var itemType = Session.get('admin.addItem.type');
+  var itemCategory = Session.get('admin.addItem.category');
+  if (itemType) {
+    $('select[name=type]').val(itemType);
+    $('select[name=category]').val(itemCategory);
   }
 });
